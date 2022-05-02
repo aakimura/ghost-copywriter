7740951755b32942cbbeac125cee2bb58cae838e
Akira Kimura <akimura@elion.ai>
2022-05-02 12:11:15 -0500

    Create copy programatically

--- generate.py
+++ generate.py
@@ -1,5 +1,57 @@
+import os
 import openai
 
+
+# Environment variables
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+PRODUCT_MODEL = os.environ.get('PRODUCT_MODEL')
+HEADLINE_MODEL = os.environ.get('HEADLINE_MODEL')
+SUBHEADLINE_MODEL = os.environ.get('SUBHEADLINE_MODEL')
+
+
+# Classes
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def create_completion(model, prompt, temperature=0.5,
+                      top_p=1,frequency_penalty=0,
+                      presence_penalty=0, stop='.\n\n'):
+    """
+    Generate a completion with a new prompt using a pre-trained model
+
+    Parameters
+    ----------
+    model: [string] ID of your pre-trained model.
+    prompt: [string] Text block containing your prompt. Remember to use the
+            same parameters as the ones you used to train your model.
+            Finish your prompt with `\n\n###\n\n`.and
+    temperature: [float] Control how creative the model should be. 1 = most
+                  creative, 0 = more 'robotic' or strict.
+
+    Returns
+    -------
+    JSON with the completion's parameters.
+    """
+    completion = openai.Completion.create(
+        model=model,
+        prompt=prompt,
+        temperature=temperature,
+        top_p=top_p,
+        frequency_penalty=frequency_penalty,
+        presence_penalty=presence_penalty,
+        stop=stop,
+        max_tokens=60)
+
+    return completion
 
 def create_prompt(company='', features='', product='', headline=''):
     """
@@ -49,36 +101,56 @@ def create_prompt(company='', features='', product='', headline=''):
 
     prompt_args.append("\n###\n\n")
 
-    return prompt_type, "\n".join(prompt_args)
+    return "\n".join(prompt_args)
 
-
-def create_completion(model, prompt, temperature=0.5,
-                      top_p=1,frequency_penalty=0,
-                      presence_penalty=0, stop='.\n\n'):
+def create_copy(company_name, features, product=None):
     """
-    Generate a completion with a new prompt using a pre-trained model
-
-    Parameters
-    ----------
-    model: [string] ID of your pre-trained model.
-    prompt: [string] Text block containing your prompt. Remember to use the
-            same parameters as the ones you used to train your model.
-            Finish your prompt with `\n\n###\n\n`.and
-    temperature: [float] Control how creative the model should be. 1 = most
-                  creative, 0 = more 'robotic' or strict.
-
-    Returns
-    -------
-    JSON with the completion's parameters.
+    Creates headline and subheadline.
     """
-    completion = openai.Completion.create(
-        model=model,
-        prompt=prompt,
-        temperature=temperature,
-        top_p=top_p,
-        frequency_penalty=frequency_penalty,
-        presence_penalty=presence_penalty,
-        stop=stop,
-        max_tokens=60)
+    # Product description creation
+    if not product:
+        print("Creating product description...", end=' ', flush=True)
+        product_prompt = create_prompt(company=company_name, features=features)
+        product = create_completion(PRODUCT_MODEL, product_prompt)
+        product = product['choices'][0]['text']
+        print("Done.")
 
-    return completion
+    # Headline creation
+    print("Creating headline...", end=' ', flush=True)
+    headline_prompt = create_prompt(company=company_name, features=features,
+                                    product=product)
+    headline = create_completion(HEADLINE_MODEL, headline_prompt)
+    headline = headline['choices'][0]['text']
+    print("Done.")
+
+    # Subheadline creation
+    print("Creating subheadline...", end=' ', flush=True)
+    subheadline_prompt = create_prompt(company=company_name, features=features,
+                                    product=product, headline=headline)
+    subheadline = create_completion(SUBHEADLINE_MODEL, subheadline_prompt)
+    subheadline = subheadline['choices'][0]['text']
+    print("Done.")
+
+    return product, headline, subheadline
+
+
+if __name__ == '__main__':
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    openai.api_key = OPENAI_API_KEY
+
+    print("\n","-"*50,"Input","-"*50)
+    company = input("What's your company name? ")
+    features = input("What are your product's features? ")
+    print("\n")
+
+    # Generate copy
+    product, headline, subheadline = create_copy(company, features)
+
+    # Results
+    print("\n","-"*50,"Results","-"*50)
+    print(f"{bcolors.HEADER}Company name:{bcolors.ENDC} {company}")
+    print(f"{bcolors.HEADER}Features:{bcolors.ENDC} {features}")
+    print(f"{bcolors.HEADER}Product description:{bcolors.ENDC} {product}")
+    print(f"{bcolors.HEADER}Headline:{bcolors.ENDC} {headline}")
+    print(f"{bcolors.HEADER}Subheadline:{bcolors.ENDC} {subheadline}")
